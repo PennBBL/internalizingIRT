aca322abff6a0e87b19aa6b6f6b1aecec6d6c4df
Ellyn Butler <butellyn@galton.uphs.upenn.edu>
2019-07-25 22:18:16 -0400

    woo

--- analyses.R
+++ analyses.R
@@ -46,18 +46,18 @@ first_proband_mod <- irt.fa(df[df$informant == "proband", grep("ITEM", colnames(
 #first_proband_mod <- irt.fa(df[df$informant == "proband", c("ITEM001", "ITEM002", "ITEM003", "ITEM004", "ITEM005")], nfactors=1, plot=FALSE)
 df[df$informant == "proband", "FirstTraitEstimate"] <- scoreIrt(first_proband_mod, df[df$informant == "proband", c("ITEM001", "ITEM002", "ITEM003", "ITEM004", "ITEM005")])$theta1
 
-summary_df <- data.frame(matrix(NA, nrow=21, ncol=length(items)+1))
-colnames(summary_df) <- c("LessThan", items)
+summary_proband_df <- data.frame(matrix(NA, nrow=21, ncol=length(items)+1))
+colnames(summary_proband_df) <- c("LessThan", items)
 splits <- seq(0, 5, .25)
-summary_df$LessThan <- splits
+summary_proband_df$LessThan <- splits
 
 prevspl <- -5
 for (item in items) {
 	prevspl <- -5
 	for (spl in splits) {
 		tmp_df <- df[df$informant == "proband" & df$FirstTraitEstimate < spl & df$FirstTraitEstimate > prevspl,]
-		if (nrow(tmp_df) == 0) { summary_df[summary_df$LessThan == spl, item] <- 0 
-		} else { summary_df[summary_df$LessThan == spl, item] <- nrow(tmp_df[tmp_df[,item] > 0,])/nrow(tmp_df)
+		if (nrow(tmp_df) == 0) { summary_proband_df[summary_proband_df$LessThan == spl, item] <- 0 
+		} else { summary_proband_df[summary_proband_df$LessThan == spl, item] <- round(nrow(tmp_df[tmp_df[,item] > 0,])/nrow(tmp_df), digits=3)*100
 		}
 		
 		prevspl <- spl
@@ -65,13 +65,60 @@ for (item in items) {
 }
 
 for (item in items) {
-	
-	tmp_plot <- ggplot() +
+	tmp_plot <- ggplot(summary_proband_df, aes_string(x="LessThan", y=item)) + 
+		geom_bar(stat="identity") + theme_minimal() + xlab("Trait Estimate Bin") +
+		ylab("Yes (%)") + ggtitle(item) + scale_y_continuous(limits=c(0,100)) +
+		theme(title = element_text(size=20))
+	assign(paste0(item, "_proband_plot"), tmp_plot)
 }
 
 
 # ii. Check that proportion of "yes" responses monotonically increase with trait score estimated using 2PL IRT model for each item for collaterals.
+first_collateral_mod <- irt.fa(df[df$informant == "collateral", grep("ITEM", colnames(df), value=TRUE)], nfactors=1, plot=FALSE)
+#first_collateral_mod <- irt.fa(df[df$informant == "collateral", c("ITEM001", "ITEM002", "ITEM003", "ITEM004", "ITEM005")], nfactors=1, plot=FALSE)
+df[df$informant == "collateral", "FirstTraitEstimate"] <- scoreIrt(first_collateral_mod, df[df$informant == "collateral", c("ITEM001", "ITEM002", "ITEM003", "ITEM004", "ITEM005")])$theta1
 
+summary_collateral_df <- data.frame(matrix(NA, nrow=21, ncol=length(items)+1))
+colnames(summary_collateral_df) <- c("LessThan", items)
+splits <- seq(0, 5, .25)
+summary_collateral_df$LessThan <- splits
+
+prevspl <- -5
+for (item in items) {
+	prevspl <- -5
+	for (spl in splits) {
+		tmp_df <- df[df$informant == "collateral" & df$FirstTraitEstimate < spl & df$FirstTraitEstimate > prevspl,]
+		if (nrow(tmp_df) == 0) { summary_collateral_df[summary_collateral_df$LessThan == spl, item] <- 0 
+		} else { summary_collateral_df[summary_collateral_df$LessThan == spl, item] <- round(nrow(tmp_df[tmp_df[,item] > 0,])/nrow(tmp_df), digits=3)*100
+		}
+		
+		prevspl <- spl
+	}
+}
+
+for (item in items) {
+	tmp_plot <- ggplot(summary_collateral_df, aes_string(x="LessThan", y=item)) + 
+		geom_bar(stat="identity") + theme_minimal() + xlab("Trait Estimate Bin") +
+		ylab("Yes (%)") + ggtitle(item) + scale_y_continuous(limits=c(0,100)) +
+		theme(title = element_text(size=20))
+	assign(paste0(item, "_collateral_plot"), tmp_plot)
+}
+
+
+pdf(file="/home/butellyn/parentchild_psychopathology/plots/monotonicEval.pdf", width=20, height=14)
+grid.arrange(ITEM001_proband_plot, ITEM002_proband_plot, ITEM003_proband_plot, ITEM004_proband_plot, ITEM005_proband_plot, ITEM006_proband_plot, nrow=2, ncol=3)
+grid.arrange(ITEM007_proband_plot, ITEM008_proband_plot, ITEM009_proband_plot, ITEM010_proband_plot, ITEM011_proband_plot, ITEM012_proband_plot, nrow=2, ncol=3)
+grid.arrange(ITEM013_proband_plot, ITEM014_proband_plot, ITEM015_proband_plot, ITEM016_proband_plot, ITEM017_proband_plot, ITEM018_proband_plot, nrow=2, ncol=3)
+grid.arrange(ITEM019_proband_plot, ITEM020_proband_plot, ITEM021_proband_plot, ITEM022_proband_plot, ITEM023_proband_plot, ITEM024_proband_plot, nrow=2, ncol=3)
+grid.arrange(ITEM025_proband_plot, ITEM026_proband_plot, ITEM027_proband_plot, ITEM028_proband_plot, ITEM029_proband_plot, ITEM030_proband_plot, nrow=2, ncol=3)
+grid.arrange(ITEM031_proband_plot, ITEM032_proband_plot, ITEM033_proband_plot, ITEM034_proband_plot, ITEM035_proband_plot, nrow=2, ncol=3)
+grid.arrange(ITEM001_collateral_plot, ITEM002_collateral_plot, ITEM003_collateral_plot, ITEM004_collateral_plot, ITEM005_collateral_plot, ITEM006_collateral_plot, nrow=2, ncol=3)
+grid.arrange(ITEM007_collateral_plot, ITEM008_collateral_plot, ITEM009_collateral_plot, ITEM010_collateral_plot, ITEM011_collateral_plot, ITEM012_collateral_plot, nrow=2, ncol=3)
+grid.arrange(ITEM013_collateral_plot, ITEM014_collateral_plot, ITEM015_collateral_plot, ITEM016_collateral_plot, ITEM017_collateral_plot, ITEM018_collateral_plot, nrow=2, ncol=3)
+grid.arrange(ITEM019_collateral_plot, ITEM020_collateral_plot, ITEM021_collateral_plot, ITEM022_collateral_plot, ITEM023_collateral_plot, ITEM024_collateral_plot, nrow=2, ncol=3)
+grid.arrange(ITEM025_collateral_plot, ITEM026_collateral_plot, ITEM027_collateral_plot, ITEM028_collateral_plot, ITEM029_collateral_plot, ITEM030_collateral_plot, nrow=2, ncol=3)
+grid.arrange(ITEM031_collateral_plot, ITEM032_collateral_plot, ITEM033_collateral_plot, ITEM034_collateral_plot, ITEM035_collateral_plot, nrow=2, ncol=3)
+dev.off()
 
 ### b) Dimensionality
 # i. Fit bifactor and unidimensional factor models to probands (create table). Then, fit a 2PL IRT model and check that the residuals in the items are not correlated (local independence).
